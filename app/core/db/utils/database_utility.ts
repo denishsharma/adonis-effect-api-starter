78a2cb07f44f412e41052799489ef8f08a9a96ee
Denish Sharma <denishcommon@gmail.com>
2025-03-19 03:14:06 +0530

    refactor: enhance DatabaseTransactionError handling with static method for unknown errors

--- app/core/db/utils/database_utility.ts
+++ app/core/db/utils/database_utility.ts
@@ -1,5 +1,4 @@
 import type { IsolationLevels } from '@adonisjs/lucid/types/database'
-import { ErrorUtility } from '#core/error_and_exception/utils/error_utility'
 import { TelemetryUtility } from '#core/telemetry/utils/telemetry_utility'
 import DatabaseTransactionError from '#errors/database_transaction_error'
 import db from '@adonisjs/lucid/services/db'
@@ -23,7 +22,7 @@ export namespace DatabaseUtility {
     return Effect.gen(function* () {
       const trx = yield* Effect.tryPromise({
         try: async () => await db.transaction(options),
-        catch: error => new DatabaseTransactionError({ operation: 'create' }, 'Unexpected error occurred while creating a database transaction.', { cause: ErrorUtility.toInternalUnknownError()(error) }),
+        catch: DatabaseTransactionError.fromUnknownError({ operation: 'create' }, 'Unexpected error occurred while creating a database transaction.'),
       })
 
       /**
@@ -40,14 +39,14 @@ export namespace DatabaseUtility {
       function commit() {
         return Effect.tryPromise({
           try: async () => await trx.commit(),
-          catch: error => new DatabaseTransactionError({ operation: 'commit' }, 'Unexpected error occurred while committing a database transaction.', { cause: ErrorUtility.toInternalUnknownError()(error) }),
+          catch: DatabaseTransactionError.fromUnknownError({ operation: 'commit' }, 'Unexpected error occurred while committing a database transaction.'),
         }).pipe(TelemetryUtility.withTelemetrySpan('commit_db_transaction'))
       }
 
       function rollback() {
         return Effect.tryPromise({
           try: async () => await trx.rollback(),
-          catch: error => new DatabaseTransactionError({ operation: 'rollback' }, 'Unexpected error occurred while rolling back a database transaction.', { cause: ErrorUtility.toInternalUnknownError()(error) }),
+          catch: DatabaseTransactionError.fromUnknownError({ operation: 'rollback' }, 'Unexpected error occurred while rolling back a database transaction.'),
         }).pipe(TelemetryUtility.withTelemetrySpan('rollback_db_transaction'))
       }
 
--- app/errors/database_transaction_error.ts
+++ app/errors/database_transaction_error.ts
@@ -1,6 +1,16 @@
+import type { TaggedInternalErrorOptions } from '#core/error_and_exception/tagged_internal_error'
 import { InternalErrorCode, InternalErrorCodeMetadata } from '#constants/internal_error_constant'
 import { TaggedInternalError } from '#core/error_and_exception/tagged_internal_error'
+import { ErrorUtility } from '#core/error_and_exception/utils/error_utility'
 import { Schema } from 'effect'
+
+/**
+ * The schema that defines the additional data context
+ * for the database transaction error.
+ */
+const DatabaseTransactionErrorSchema = Schema.Struct({
+  operation: Schema.Literal('create', 'commit', 'rollback'),
+})
 
 /**
  * Error occurs when there was an unexpected error while performing
@@ -9,7 +19,20 @@ import { Schema } from 'effect'
 export default class DatabaseTransactionError extends TaggedInternalError('database_transaction')({
   code: InternalErrorCode.I_DB_TRANSACTION_ERROR,
   message: InternalErrorCodeMetadata[InternalErrorCode.I_DB_TRANSACTION_ERROR].message,
-  schema: Schema.Struct({
-    operation: Schema.Literal('create', 'commit', 'rollback'),
-  }),
-}) {}
+  schema: DatabaseTransactionErrorSchema,
+}) {
+  /**
+   * Creates a new database transaction error
+   * instance from an unknown error.
+   *
+   * @param data The additional data context for the error.
+   * @param message The error message.
+   * @param options The error options.
+   */
+  static fromUnknownError(data: Schema.Schema.Encoded<typeof DatabaseTransactionErrorSchema>, message?: string, options?: Omit<TaggedInternalErrorOptions, 'cause'>) {
+    /**
+     * @param error The unknown error that caused the database transaction error.
+     */
+    return (error: unknown) => new DatabaseTransactionError(data, message, { ...options, cause: ErrorUtility.toInternalUnknownError()(error) })
+  }
+}
